b72afffa745c1cabe495e5078dbf0046c562cd3f
Done57 <done57@github.com>
2026-07-13 05:53:30 +0800

    更新配置和脚本

--- src/nowin.sh
+++ src/nowin.sh
@@ -11,17 +11,13 @@ if [ -f "$CONFIG_FILE" ]; then
 fi
 
 # 默认参数
-SPEED=${SPEED:-0.01}
-LINES=${LINES:-10000}
+SPEED=${SPEED:-0.00000001}
+LINES=${LINES:-100000000000}
 COLOR=${COLOR:-32}
 
 # 刷屏
 for ((i=0; i<$LINES; i++)); do
     echo -e "\033[${COLOR}mNo Windows, Yes Linux!\033[0m"
-    # 随机彩蛋（10% 概率）
-    if [ $((RANDOM % 10)) -eq 0 ]; then
-        echo -e "\033[31m【净化】检测到 .exe 异端，启动清除程序...\033[0m"
-    fi
     sleep $SPEED
 done
 
